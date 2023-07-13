9a3ee2413cbceb3d4b5ba8be4baecae3f3f754cd
Renan Galdino Truta <115110286+galdinorenantruta@users.noreply.github.com>
2023-07-12 21:52:04 -0300

    First Commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Trips from "./components/Trips";
+
+const page = () => {
+  return (
+    <div>
+      <div>page</div>
+      <Trips />
+    </div>
+  );
+};
+
+export default page;
