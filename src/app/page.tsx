abd485aff101eaf0ef9ed30fa56712ab8e16df3d
Renan Galdino Truta <115110286+galdinorenantruta@users.noreply.github.com>
2023-07-13 08:37:26 -0300

    feat: add next auth provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
-  return <div>Home</div>;
+  const { data } = useSession();
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
